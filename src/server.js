e857abe27f69dfda8b7337db95637e8e2376fd20
Schweizer_KL <schweizer23.ss@gmail.com>
2026-05-29 16:40:47 +0800

    Naa koi gidungag

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
+console.log('Wow amazing! Updated ka na ba?')
+
 server.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 })
