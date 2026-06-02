bb1b0a635173992966cd5e27e8e9addb813f702b
bebot <kimjeebillones@gmail.com>
2026-06-02 15:54:04 +0800

    model signup done

---
+++ src/models/login/signup.js
@@ -0,0 +1,14 @@
+const express = require('express') 
+const db = require('../../_db/db_functions')
+
+const signupModel = (req_body) => {
+    const { id, username, name, password } = req.body
+
+    const response = db.addClient(id, username, name, password)
+
+    return response;
+
+}
+
+module.export = signupModel;
+
