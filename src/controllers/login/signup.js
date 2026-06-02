4d384565747eb8b96ae2266a91ce74065f1ef233
Reynante Billones <reynantebillones@192.168.1.6>
2026-06-02 15:52:50 +0800

    Controller and router

---
+++ src/controllers/login/signup.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+// const singupModel = require('../../models/login/signup')
+
+const singupController = (req, res) => {
+    const { id, name, username, password } = req.body;
+
+    if (!id || !name || !username || !password) return res.status(401).json({ success: false, message: "Invalid!" })
+
+        const response = signupModel(req.body);
+
+        if (response) {
+        res.status(200).json({ success: true, message: "Success!" })
+    } else {
+        res.status(400).json({ success: false, message: "Error!" })
+    }
+}
+
+module.exports = singupController;
--- src/routers/login.js
+++ src/routers/login.js
@@ -3,5 +3,6 @@ const express = require('express');
 const loginRouter = express.Router();
 
 loginRouter.post('/login', require('../controllers/login/login'))
+loginRouter.post('/signup', require('../controllers/login/signup'))
 
 module.exports = loginRouter;
