3ea0730e9ae89271f4d7b3b7e2bde47bf85e2b8f
bebot <kimjeebillones@gmail.com>
2026-06-04 16:29:29 +0800

    fix ra jud salamat

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// })
+
+app.use(cors({
+    origin: "http://localhost:5500",
+    credentials: true
+    
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
